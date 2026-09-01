81723a5d2d7ce802be789c442bcbd23aa9e32753
abdulgafarabdurrasheed <narutoshippudeen124@gmail.com>
2026-09-01 15:57:26 +0100

    Added sattellite coordinates updates in the browser

--- vanguard-ui/src/App.tsx
+++ vanguard-ui/src/App.tsx
@@ -1,8 +1,29 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import './index.css'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [coordinates, setCoordinates] = useState<[number, number, number]>([0, 0, 0]);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://127.0.0.1:3000/ws");
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log("Incoming from Rust:", data);
+      setCoordinates([
+        data.predicted_xyz[0] / 6371, 
+        data.predicted_xyz[1] / 6371, 
+        data.predicted_xyz[2] / 6371
+      ]);
+    };
+
+    return () => {
+      socket.close();
+    }
+
+  }, []);
+
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
@@ -10,6 +31,10 @@ function App() {
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial color={'#4a90e2'} />
+      </mesh>
+      <mesh position={coordinates}>
+        <sphereGeometry args={[0.1, 32, 32]} />
+        <meshPhongMaterial color={'#e24a4a'} />
       </mesh>
       <OrbitControls />
     </Canvas>
